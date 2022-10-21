9ec5b61a621ffb43d377b8d9a8375370fbfa0d88
leleking <simeonnortey@gmail.com>
2022-10-21 11:54:15 +0000

    CPROD-1244 setup ic added idl files

---
+++ src/shared/constants.ts
@@ -0,0 +1,3 @@
+export const IC_HOST = process.env.REACT_APP_IC_HOST ?? "http://localhost:8000";
+export const IC_ENVIRON = process.env.REACT_APP_IC_ENVIRON;
+export const CANISTER_IDS_URL = process.env.REACT_APP_CANISTER_IDS_URL ?? "";
