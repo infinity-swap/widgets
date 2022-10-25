0b5c0a064cf8d48d2bc7b554308447a176edc79c
leleking <simeonnortey@gmail.com>
2022-10-25 08:45:23 +0000

    CPROD-1244 added utls

---
+++ src/utils/index.ts
@@ -0,0 +1,27 @@
+import { ReactComponent as PlugIcon } from "../assets/svg/wallets/plug.svg";
+import { ReactComponent as InfinityWalletIcon } from "../assets/svg/wallets/plug.svg";
+import { walletType } from "../types";
+
+export const WALLET_IDS = {
+  PLUG: "plug",
+  INFINITY_WALLET: "infinityWallet",
+};
+
+export const WALLETS: walletType[] = [
+  {
+    id: WALLET_IDS.INFINITY_WALLET,
+    exposedName: "infinityWallet",
+    name: "InfinityWallet",
+    Icon: InfinityWalletIcon,
+    installation:
+      "https://chrome.google.com/webstore/detail/infinity-wallet/jnldfbidonfeldmalbflbmlebbipcnle",
+  },
+  {
+    id: WALLET_IDS.PLUG,
+    exposedName: "plug",
+    name: "Plug",
+    Icon: PlugIcon,
+    installation: "https://plugwallet.ooo/",
+    disabled: true,
+  },
+];
