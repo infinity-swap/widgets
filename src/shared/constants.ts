115229f822771534eb23d564c953c0c61f6a7403
leleking <simeonnortey@gmail.com>
2022-10-28 09:39:43 +0000

    CPROD-1250 added Mainnet ledger canister id

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,7 @@
 export const IC_HOST = process.env.REACT_APP_IC_HOST ?? "http://localhost:8000";
 export const IC_ENVIRON = process.env.REACT_APP_IC_ENVIRON;
 export const CANISTER_IDS_URL = process.env.REACT_APP_CANISTER_IDS_URL ?? "";
+
+export const MAINNET_LEDGER_CANISTER_ID =
+  process.env.REACT_APP_MAINNET_LEDGER_CANISTER_ID ??
+  "ryjl3-tyaaa-aaaaa-aaaba-cai";
