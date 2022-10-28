5b47e7822d43ade2f18a8936a99028272b1db080
leleking <simeonnortey@gmail.com>
2022-10-28 09:47:28 +0000

    CPROD-1250 added app wrapper for providers

---
+++ src/components/AppWrapper.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ConnectWalletProvider } from "../contexts/ConnectWallet";
+import { InstallPromptProvider } from "../contexts/InstallPrompt";
+import { UserWalletProvider } from "../contexts/UserWallet";
+import { getRejectErrorCode } from "../utils/canisterErrorHandler";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+      retry: (failureCount, error) => {
+        const icError = getRejectErrorCode(error);
+
+        if (icError && !icError.retry) {
+          if (icError.notify) {
+            //errorNotify(icError.message);
+          }
+
+          return false;
+        }
+
+        return failureCount < 5;
+      },
+    },
+  },
+});
+
+function AppWrapper({ children }: { children: JSX.Element }) {
+  return (
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <UserWalletProvider>
+          <ConnectWalletProvider>
+            <InstallPromptProvider>{children}</InstallPromptProvider>
+          </ConnectWalletProvider>
+        </UserWalletProvider>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default AppWrapper;
