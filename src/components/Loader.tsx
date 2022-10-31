928435745b4b62c11f93f5a6ed0fdfb29de0eec3
leleking <simeonnortey@gmail.com>
2022-10-31 12:55:11 +0000

    CPROD-1250 added height and width props

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import SpinningLoaderDark from "../assets/gif/loader-light.gif";
 
-export default function Loader() {
+export default function Loader({
+  height = 20,
+  width = 20,
+}: {
+  height?: number;
+  width?: number;
+}) {
   return (
     <div>
-      <img src={SpinningLoaderDark} alt="loader" height={20} width={20} />
+      <img
+        src={SpinningLoaderDark}
+        alt="loader"
+        height={height}
+        width={width}
+      />
     </div>
   );
 }
