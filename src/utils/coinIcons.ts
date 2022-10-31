b2597f42955d200e55dc546502847c683ba6eeb2
leleking <simeonnortey@gmail.com>
2022-10-31 17:41:27 +0000

    CPROD-1250 added t-icp icon in coinIcons

--- src/utils/coinIcons.ts
+++ src/utils/coinIcons.ts
@@ -2,4 +2,6 @@ export const CoinIcons: any = {
   aave: "https://raw.githubusercontent.com/infinity-swap/cryptocurrency-icons/master/16/aave.png",
   bnb: "https://raw.githubusercontent.com/infinity-swap/cryptocurrency-icons/master/16/bnb.png",
   icp: "https://raw.githubusercontent.com/infinity-swap/cryptocurrency-icons/master/16/internet-computer.png",
+  "t-icp":
+    "https://raw.githubusercontent.com/infinity-swap/cryptocurrency-icons/master/16/internet-computer.png",
 };
