3dc2784559b23c454226ec728714768e2ac0c8f3
leleking <simeonnortey@gmail.com>
2022-11-09 14:50:44 +0000

    CPROD-1252 added examples on widget

--- src/stories/Widget.stories.tsx
+++ src/stories/Widget.stories.tsx
@@ -15,17 +15,28 @@ const changeTheme = () => {
   theme = { textDark: "red" };
 };
 
+const accountId =
+  "2d628dd4eb5e1ece469ee49473d8556a2899973f1c7ea3b70c5d442a9713cc7b";
+const principalId =
+  "lobus-dmxw4-q4hcu-booky-76n5a-lpgcv-2bhd2-uihsp-cn4o5-nofux-jqe";
 export const Widget = () => (
   <div>
     <SwapWidget
       theme={theme}
-      onConnectWallet={() => onConnectWallet()}
+      // onConnectWallet={() => onConnectWallet()}
+      //accountId = {accountId}
+      // principalId = principalId=""
       icNetwork={{
-        icHost: "http://localhost:8002",
+        icHost: "http://localhost:8000",
         icEnviron: "local",
         MAINNET_LEDGER_CANISTER_ID: "ryjl3-tyaaa-aaaaa-aaaba-cai",
         CANISTER_IDS_URL: "http://localhost:8001/static/canister_ids.json",
       }}
+      defaultInputAmount="1"
+      defaultOutputTokenSymbol="TKN1"
+      defaultInputTokenSymbol="T-ICP"
+      onSuccess={() => console.log("it a successful swap")}
+      onError={() => console.log("oh no there was an error")}
     />
   </div>
 );
