4ba453e7a311b112db2c22ad215100290b5167b8
leleking <simeonnortey@gmail.com>
2022-12-12 22:13:15 +0000

    CPROD-1239 added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="font-medium text-xs text-[var(--textSecondary)]">
+      <div className="font-medium text-xs text-[var(--textSecondary)] whitespace-nowrap">
         Powered by the InfinitySwap protocol
       </div>
     </div>
