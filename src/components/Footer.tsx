9448cbf92f1d6e983e16047e8dd3e8febe8417ce
leleking <simeonnortey@gmail.com>
2022-12-12 15:16:54 +0000

    CPROD-1239 added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="font-medium text-xs text-[var(--textSecondary)]">
+        Powered by the InfinitySwap protocol
+      </div>
+    </div>
+  );
+};
